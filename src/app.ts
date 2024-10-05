6f2967928348bfb698576b92319109e3a0896604
Vitor Batista <42703631+vitorsantosb@users.noreply.github.com>
2024-10-05 11:27:06 -0300

    Update: ts config json, and added new dependency and remove files

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,49 @@
-import express, { Application } from "express";
 
-//Initialize Express App
-const app: Application = express();
+import express from 'express';
+import { Request, Response, NextFunction } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const app = express();
 
-app.use("/", (req, res) => {
-    res.json({ message: "Server up and running" });
+app.use(cors({
+    origin: ['*'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+}))
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+    limit: '50mb',
+    parameterLimit: 50000
+}));
+
+app.use((_req: Request, res: Response, next: NextFunction): void => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (_req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        res.status(200).json({});
+    } else {
+        next();
+    }
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    const err = new Error('Not Found');
+    //err.status = 404;
+    next(err);
+});
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    //res.status();
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
 });
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
-import "./server";
+import app from "./app";
+import * as http from "node:http";
+import {DebugLogMessage} from "@configs/logs/logMessages";
 
 
+async function StartHTTPServer(): Promise<void> {
+  const port: number = parseInt(process.env.PORT as string, 10) || 5000;
+  const server: http.Server = http.createServer(app);
 
+  return new Promise((resolve) => {
+    server.listen(port, () => {
+      DebugLogMessage('[SERVER]', 'routes is now enabled on port: ' + port);
+      resolve();
+    });
+  });
+}
 
 
 async function init() {
-    try {
-
-
-    } catch (error) {
-        console.error("Error during initialization", error);
-        process.exit(1);
-    }
+  try {
+    await StartHTTPServer();
+  } catch (error) {
+    console.error("Error during initialization", error);
+    process.exit(1);
+  }
 }
 
 init().catch(err => console.error(err));
